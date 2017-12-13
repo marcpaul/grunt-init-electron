0e6dd38dcaba0acc1fd2db84a1f1f1b9c66e0a61
marcpaul <marcpaul@users.noreply.github.com>
2017-12-13 21:15:56 +0100

    added deepmerge

--- template.js
+++ template.js
@@ -1,19 +1,22 @@
+const deepmerge = require('deepmerge')
+
 exports.description = 'Setup grunt, linter, editorconfig etc in your electron-forge project'
 
 exports.notes = 'Make sure you initialized your project with _electron-forge init projectname_.'
-
-// exports.warnOn = '*'
 
 exports.template = function(grunt, init, done) {
   init.process({}, [
     init.prompt('description'),
     init.prompt('repository'),
+    init.prompt('author_name'),
+    init.prompt('homepage'),
+    init.prompt('licenses')
 
   ], function(err, props) {
 
     props.devDependencies = {
       'stylelint': 'latest',
-      'stylelint-config-standard': 'latest'
+      'stylelint-config-standard': 'latest',
       'eslint-config-standard': 'latest',
       'eslint-plugin-html': 'latest',
       'eslint-plugin-import': 'latest',
@@ -25,11 +28,15 @@ exports.template = function(grunt, init, done) {
       'eslint-plugin-standard': 'latest'
     }
 
-    const pkg = grunt.file.readJSON('./back/package.json')
+    const files = init.filesToCopy(props)
+    init.addLicenseFiles(files, props.licenses)
+    init.copyAndProcess(files, props)
 
-    const test = Object.assign(pkg, props)
+    const pkg = grunt.file.readJSON('./package.json')
+    delete pkg.devDependencies['eslint-config-airbnb']
 
-    init.writePackageJSON('package.json', test)
+    const newPackage = deepmerge(pkg, props)
+    init.writePackageJSON('package.json', newPackage)
 
     done()
   })
