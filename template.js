22e3a84479882d79c7b8ae4f656b06f092909d06
marcpaul <marcpaul@users.noreply.github.com>
2017-12-14 19:00:14 +0100

    added electron icons creator

--- template.js
+++ template.js
@@ -6,13 +6,23 @@ exports.notes = 'Make sure you initialized your project with _electron-forge ini
 
 exports.template = function(grunt, init, done) {
   init.process({}, [
-    // init.prompt('description'),
-    // init.prompt('repository'),
-     init.prompt('author_name'),
-    // init.prompt('homepage'),
-    // init.prompt('licenses')
-
+    init.prompt('description'),
+    {
+      name: 'travis',
+      message: 'Will this project be tested with Travis CI?',
+      default: 'Y/n',
+      warning: 'If selected, you must enable Travis support for this project in https://travis-ci.org/profile'
+    },
+    {
+      name: 'snyk',
+      message: 'Add snyk for vulnerability testing?',
+      default: 'Y/n',
+      warning: 'If selected, you must enable project on snyk'
+    }
   ], function(err, props) {
+    const target = grunt.option('target')
+    props.travis = /y/i.test(props.travis)
+    props.snyk = /y/i.test(props.snyk)
 
     props.devDependencies = {
       'stylelint': 'latest',
@@ -28,16 +38,27 @@ exports.template = function(grunt, init, done) {
       'eslint-plugin-standard': 'latest'
     }
 
+    if (props.snyk) {
+      Object.assign(props.devDependencies, {'snyk': 'latest'})
+    }
+
     const files = init.filesToCopy(props)
-    init.addLicenseFiles(files, ['MIT'])
     init.copyAndProcess(files, props)
 
-    const pkg = grunt.file.readJSON('./package.json')
-    delete pkg.devDependencies['eslint-config-airbnb']
-
-    grunt.file.delete('node_modules')
-    const newPackage = deepmerge(pkg, props)
-    init.writePackageJSON('package.json', newPackage, () => {return newPackage})
+    if (target === 'electron') {
+      const pkg = grunt.file.readJSON('./package.json')
+      pkg.config.forge.github_repository.owner = pkg.author
+      pkg.config.forge.github_repository.name = pkg.name
+      pkg.scripts.start = 'ELECTRON_IS_DEV=true electron-forge start'
+      delete pkg.devDependencies['eslint-config-airbnb']
+      delete props.travis
+      delete props.snyk
+      if (grunt.file.exists('node_modules')) {
+        grunt.file.delete('node_modules')
+      }
+      const newPackage = deepmerge(pkg, props)
+      init.writePackageJSON('package.json', newPackage, () => {return newPackage})
+    }
 
     done()
   })
